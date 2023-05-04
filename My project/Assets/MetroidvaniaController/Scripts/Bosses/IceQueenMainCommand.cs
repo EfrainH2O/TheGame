11300df6e0af2fdb35c66415e92e58b46fd0ee1b
EfrainH2O <112781053+EfrainH2O@users.noreply.github.com>
2023-05-04 15:54:09 -0600

    Definiendo las bases para la eleccion de acciones dentro de lo alatorio
    Definiendo las bases para la eleccion de acciones dentro de lo alatorio

--- My project/Assets/MetroidvaniaController/Scripts/Bosses/IceQueenMainCommand.cs
+++ My project/Assets/MetroidvaniaController/Scripts/Bosses/IceQueenMainCommand.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IceMovementMainCommand : MonoBehaviour
+public class IceQueenMainCommand: MonoBehaviour
 {
     private int life = 6;
     private Vector3 Spawnpoint;
@@ -11,18 +11,26 @@ public class IceMovementMainCommand : MonoBehaviour
 
 
     public GameObject Player;
+    private GameObject Canon;
+    private GameObject ACanon;
+    private GameObject attackCheck;
 
     public float TimeInIdle;
+    public float TimeInSingleShoot;
+    public float TimeInCentripetalShoot;
+
+
     public float speed;
-    private bool OnLongRange;
+    public float distance;
+   
     private bool OnShortRange;
     private bool CanDVD;
 
 
     private bool ReadyDecision;
-    private int Decision;
+    private int  Decision;
     private bool par;
-
+    private bool CanMove;
 
 
 
@@ -34,14 +42,19 @@ public class IceMovementMainCommand : MonoBehaviour
         Spawnpoint = transform.position;
         ReadyDecision = false;
         facingRight = false;
+        CanMove = false;
+        Canon = transform.GetChild(0).gameObject;
+        ACanon = transform.GetChild(1).gameObject;
 
+        Canon.SetActive(false);
+        ACanon.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-
+        Flipo();
 
         if (!ReadyDecision)
         {
@@ -67,12 +80,13 @@ public class IceMovementMainCommand : MonoBehaviour
             {
                 Fase3();
             }
-            if (life <= 0)
+            else if (life <= 0)
             {
                 Dead();
             }
-            StartCoroutine(WaitToEnd(TimeInIdle));
+            
         }
+        Respawn();
     }
 
 
@@ -100,12 +114,10 @@ public class IceMovementMainCommand : MonoBehaviour
         
     }
 
-
-
-    void MeleeAttack()
+    public IEnumerator MeleeAttack ()
     {
-        
-        transform.GetComponent<Animator>().SetBool("Attack", true);
+
+        transform.GetComponent<Animator>().SetBool("Slash", true);
         Collider2D[] collidersEnemies = Physics2D.OverlapCircleAll(attackCheck.position, 0.9f);
         for (int i = 0; i < collidersEnemies.Length; i++)
         {
@@ -114,13 +126,83 @@ public class IceMovementMainCommand : MonoBehaviour
                 collidersEnemies[i].gameObject.GetComponent<CharacterController2D>().ApplyDamage(transform.position);
             }
         }
-    
+        yield return new WaitForSeconds(1.3f);
+        CanMove = true;
+
     }
 
 
     IEnumerator WaitToEnd(float delay)
     {
+        Flipo();
         yield return new WaitForSeconds(delay);
         ReadyDecision = false;
     }
+
+
+    IEnumerator SingleBall(float delay)
+    {   
+        CanMove = false;
+        // Animation.setBool ( SingleBall, true )
+        ACanon.SetActive(true);
+        yield return new WaitForSeconds(delay);
+        ACanon.SetActive(false);
+        // Animation.setBool ( SingleBall, false )
+        CanMove = true;
+
+        StartCoroutine(WaitToEnd(TimeInIdle));
+
+    }
+
+    IEnumerator CentripetalBalls(float delay)
+    {
+
+        CanMove = false;
+        // Animation.setBool ( CentripetalBalll, true )
+        Canon.SetActive(true);
+        yield return new WaitForSeconds(delay);
+        Canon.SetActive(false);
+        // Animation.setBool ( CentripetalBall, false )
+        CanMove = true;
+
+        StartCoroutine(WaitToEnd(TimeInIdle));
+        
+    }
+
+    void Slash ()
+    {
+        CanMove = false;
+        MeleeAttack();
+        StartCoroutine(WaitToEnd(TimeInIdle));
+
+    }
+
+    IEnumerator Dash()
+    {
+
+    }
+
+    void Move( bool can, float speed )
+    {
+        if (can)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    void Respawn()
+    {
+        if (Player.GetComponent<CharacterController2D>().CanReapear)
+        {
+            transform.position = Spawnpoint;
+            life = 6;
+            //Animator.setBool (cadaUno, false);
+        }
+    }
+
+
 }
